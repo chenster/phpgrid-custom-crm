28865e407c013e4b5acd02377a3147cd92e41c51
phpcontrols <richard@phpcontrols.com>
2017-02-24 14:08:20 +0800

    Tag

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <br /><br /><br /><br /><br />
-<div class="footer">phpGrid - <strong>Build From Scratch Series</strong>. &copy; <?php echo date('Y'); ?>.</div>
+<div class="footer"></div>
 
 </body>
 </html>
